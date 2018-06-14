3efa132e7e06624efa7ac7261596b64bd9944e1d
James Gornet <james.gornet@columbia.edu>
2018-06-14 16:49:35 -0700

    Added tests for LossWriter and TrainingLogger

--- tests/test_core.py
+++ tests/test_core.py
@@ -2,7 +2,10 @@ import unittest
 from neurotorch.core.Trainer import Trainer
 from neurotorch.nets.RSUNet import RSUNet
 from neurotorch.datasets.TiffDataset import TiffDataset
+from neurotorch.visualization.TensorboardWriter import (LossWriter,
+                                                        TrainingLogger)
 import os.path
+import pytest
 
 IMAGE_PATH = "./tests/images"
 
@@ -18,6 +21,7 @@ class TestTrainer(unittest.TestCase):
                           gpu_device=0)
         trainer.run_training()
 
+    @pytest.mark.skip()
     def test_cpu_training(self):
         net = RSUNet()
         inputs_dataset = TiffDataset(os.path.join(IMAGE_PATH,
@@ -25,4 +29,26 @@ class TestTrainer(unittest.TestCase):
         labels_dataset = TiffDataset(os.path.join(IMAGE_PATH,
                                                   "labels.tif"))
         trainer = Trainer(net, inputs_dataset, labels_dataset, max_epochs=2)
+        trainer.run_training()
+
+    def test_loss_writer(self):
+        net = RSUNet()
+        inputs_dataset = TiffDataset(os.path.join(IMAGE_PATH,
+                                                  "sample_volume.tif"))
+        labels_dataset = TiffDataset(os.path.join(IMAGE_PATH,
+                                                  "labels.tif"))
+        trainer = Trainer(net, inputs_dataset, labels_dataset, max_epochs=2,
+                          gpu_device=0)
+        trainer = LossWriter(trainer, './tests/')
+        trainer.run_training()
+
+    def test_training_logger(self):
+        net = RSUNet()
+        inputs_dataset = TiffDataset(os.path.join(IMAGE_PATH,
+                                                  "sample_volume.tif"))
+        labels_dataset = TiffDataset(os.path.join(IMAGE_PATH,
+                                                  "labels.tif"))
+        trainer = Trainer(net, inputs_dataset, labels_dataset, max_epochs=2,
+                          gpu_device=0)
+        trainer = TrainingLogger(trainer)
         trainer.run_training()
