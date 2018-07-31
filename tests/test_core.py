fb35b93cfdfc5a467239d694ea68ba1d4f0d98d6
James Gornet <gornet@columbia.edu>
2018-07-30 18:37:25 -0700

    Added unit tests for training

--- tests/test_core.py
+++ tests/test_core.py
@@ -6,6 +6,7 @@ from neurotorch.training.logging import (LossWriter,
                                          TrainingLogger)
 from neurotorch.training.checkpoint import CheckpointWriter
 import os.path
+import os
 import shutil
 import pytest
 
@@ -13,7 +14,6 @@ IMAGE_PATH = "./tests/images"
 
 
 class TestTrainer(unittest.TestCase):
-    @pytest.mark.skip()
     def test_gpu_training(self):
         net = RSUNet()
         inputs_dataset = TorchVolume(TiffVolume(os.path.join(IMAGE_PATH,
@@ -34,8 +34,9 @@ class TestTrainer(unittest.TestCase):
         trainer = Trainer(net, inputs_dataset, labels_dataset, max_epochs=1)
         trainer.run_training()
 
-    @pytest.mark.skip()
     def test_loss_writer(self):
+        if not os.path.isdir('./tests/test_experiment'):
+            os.mkdir('tests/test_experiment')
         shutil.rmtree('./tests/test_experiment')
 
         net = RSUNet()
@@ -43,33 +44,38 @@ class TestTrainer(unittest.TestCase):
                                                  "sample_volume.tif"))
         labels_dataset = TiffVolume(os.path.join(IMAGE_PATH,
                                                  "labels.tif"))
-        trainer = Trainer(net, inputs_dataset, labels_dataset, max_epochs=1)
+        trainer = Trainer(net, inputs_dataset, labels_dataset, max_epochs=1,
+                          gpu_device=0)
         trainer = LossWriter(trainer, './tests/', "test_experiment")
         trainer.run_training()
 
-    @pytest.mark.skip()
     def test_training_logger(self):
         net = RSUNet()
         inputs_dataset = TiffVolume(os.path.join(IMAGE_PATH,
                                                  "sample_volume.tif"))
         labels_dataset = TiffVolume(os.path.join(IMAGE_PATH,
                                                  "labels.tif"))
-        trainer = Trainer(net, inputs_dataset, labels_dataset, max_epochs=1)
+        trainer = Trainer(net, inputs_dataset, labels_dataset, max_epochs=1,
+                          gpu_device=0)
         trainer = TrainingLogger(trainer, logger_dir='.')
         trainer.run_training()
 
-    @pytest.mark.skip()
     def test_checkpoint(self):
+        if not os.path.isdir('./tests/checkpoints'):
+            os.mkdir('tests/checkpoints')
+
         net = RSUNet()
         inputs_dataset = TiffVolume(os.path.join(IMAGE_PATH,
                                                  "sample_volume.tif"))
         labels_dataset = TiffVolume(os.path.join(IMAGE_PATH,
                                                  "labels.tif"))
-        trainer = Trainer(net, inputs_dataset, labels_dataset, max_epochs=10)
+        trainer = Trainer(net, inputs_dataset, labels_dataset, max_epochs=10,
+                          gpu_device=0)
         trainer = CheckpointWriter(trainer,
                                    checkpoint_dir='./tests/checkpoints',
                                    checkpoint_period=5)
         trainer.run_training()
         trainer = Trainer(net, inputs_dataset, labels_dataset, max_epochs=10,
-                          checkpoint='./tests/checkpoints/iteration_5.ckpt')
+                          checkpoint='./tests/checkpoints/iteration_5.ckpt',
+                          gpu_device=0)
         trainer.run_training()
