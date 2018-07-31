a0e728a4b6e41a3fb669184dfec5d00e0b3d39b5
James Gornet <gornet@columbia.edu>
2018-07-30 17:32:48 -0700

    Added an unit test for an occlusion augmentation

--- tests/test_augmentations.py
+++ tests/test_augmentations.py
@@ -25,3 +25,19 @@ class TestAugmentations(unittest.TestCase):
                    brightness_dataset[10][0].getArray())
         tif.imsave(os.path.join(IMAGE_PATH, "test_brightness_label.tif"),
                    brightness_dataset[10][1].getArray()*255)
+
+    def test_occlusion(self):
+        input_dataset = TiffVolume(os.path.join(IMAGE_PATH,
+                                                "sample_volume.tif"))
+        label_dataset = TiffVolume(os.path.join(IMAGE_PATH,
+                                                "labels.tif"))
+        training_dataset = AlignedVolume((input_dataset, label_dataset),
+                                         iteration_size=BoundingBox(Vector(0, 0, 0), Vector(128, 128, 20)),
+                                         stride=Vector(128, 128, 20))
+
+        occlusion_dataset = Occlusion(training_dataset)
+
+        tif.imsave(os.path.join(IMAGE_PATH, "test_occlusion_input.tif"),
+                   occlusion_dataset[10][0].getArray())
+        tif.imsave(os.path.join(IMAGE_PATH, "test_occlusion_label.tif"),
+                   occlusion_dataset[10][1].getArray()*255)
