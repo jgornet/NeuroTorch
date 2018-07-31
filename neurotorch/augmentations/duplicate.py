2dbadf8ad063f049d19236751bc7ee6b43d47c25
James Gornet <gornet@columbia.edu>
2018-07-30 18:14:12 -0700

    Added a slice duplication augmentation

---
+++ neurotorch/augmentations/duplicate.py
@@ -0,0 +1,45 @@
+from neurotorch.augmentations.augmentation import Augmentation
+from neurotorch.datasets.volumedataset import Data
+import random
+import numpy as np
+
+
+class Duplicate(Augmentation):
+    def __init__(self, volume, frequency=0.1, max_slices=2):
+        self.setFrequency(frequency)
+        self.setMaxSlices(max_slices)
+        super().__init__(volume)
+
+    def augment(self, bounding_box):
+        slices = self.getSlices()
+        end = bounding_box.getSize().getComponents[0]
+        location = random.randrange(slices-end)
+
+        raw_data = self.getInput(bounding_box)
+        label_data = self.getLabel(bounding_box)
+        augmented_raw, augmented_label = self.duplication(raw_data, label_data,
+                                                          location=location,
+                                                          slices=slices)
+
+        return (augmented_raw, augmented_label)
+
+    def setFrequency(self, frequency):
+        self.frequency = frequency
+
+    def setMaxSlices(self, max_slices):
+        self.max_slices = max_slices
+
+    def getMaxSlices(self):
+        return max_slices
+
+    def getSlices(self):
+        return random.randrange(self.getMaxSlices)
+
+    def duplication(self, raw_data, label_data,
+                                 location=20, slices=3):
+        raw = raw_data.getArray()
+        distorted_raw = raw.copy()
+        duplicate_slices = np.repeat(raw[location, :, :].reshape(1, raw.shape[1], raw.shape[2]), slices, axis=0)
+        distorted_raw[location:location+slices, :, :] = duplicate_slices
+
+    return distorted_raw, label
--- tests/test_augmentations.py
+++ tests/test_augmentations.py
@@ -1,5 +1,6 @@
 from neurotorch.augmentations.brightness import Brightness
 from neurotorch.augmentations.occlusion import Occlusion
+from neurotorch.augmentations.duplicate import Duplicate
 import unittest
 from neurotorch.datasets.volumedataset import TiffVolume, AlignedVolume
 import tifffile as tif
@@ -42,3 +43,19 @@ class TestAugmentations(unittest.TestCase):
                    occlusion_dataset[10][0].getArray())
         tif.imsave(os.path.join(IMAGE_PATH, "test_occlusion_label.tif"),
                    occlusion_dataset[10][1].getArray()*255)
+
+    def test_duplication(self):
+        input_dataset = TiffVolume(os.path.join(IMAGE_PATH,
+                                                "sample_volume.tif"))
+        label_dataset = TiffVolume(os.path.join(IMAGE_PATH,
+                                                "labels.tif"))
+        training_dataset = AlignedVolume((input_dataset, label_dataset),
+                                         iteration_size=BoundingBox(Vector(0, 0, 0), Vector(128, 128, 20)),
+                                         stride=Vector(128, 128, 20))
+
+        duplicate_dataset = Duplicate(training_dataset)
+
+        tif.imsave(os.path.join(IMAGE_PATH, "test_duplicate_input.tif"),
+                   duplicate_dataset[10][0].getArray())
+        tif.imsave(os.path.join(IMAGE_PATH, "test_duplicate_label.tif"),
+                   duplicate_dataset[10][1].getArray()*255)
