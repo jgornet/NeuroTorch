e3821c9a1de7dc02227b5ee29d2b08f905b41ad9
James Gornet <james.gornet@columbia.edu>
2018-08-29 19:22:07 -0700

    Changed prediction output to 16-bit TIFF

--- tests/test_core.py
+++ tests/test_core.py
@@ -114,9 +114,9 @@ class TestTrainer(unittest.TestCase):
         predictor.run(inputs_dataset, output_volume, batch_size=5)
 
         array = output_volume.getArray().astype(np.float32)
-        array = 255/(1-np.exp(-array+0.5))
+        array = (array - np.min(array))*65535/(np.max(array)-np.min(array))
 
         tif.imsave(os.path.join(IMAGE_PATH,
                                 "test_prediction.tif"),
-                   array.astype(np.uint8),
+                   array.astype(np.uint16),
                    compress=6)
