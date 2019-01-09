8a94f16f456636ee9d0c99c278fd17138b41778c
James Gornet <jamesgornet@dyn-160-39-182-144.dyn.columbia.edu>
2019-01-09 17:41:09 -0500

    Updated setup.py

--- setup.py
+++ setup.py
@@ -1,8 +1,8 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="NeuroTorch",
-    version="0.0.1",
+    name="neurotorch",
+    version="0.1.0",
     packages=find_packages(),
     setup_requires=["pytest-runner"],
     tests_require=["pytest"],
