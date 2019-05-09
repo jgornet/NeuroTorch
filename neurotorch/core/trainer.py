9541b83ad4dd6d682b63185dadddd57363f19a70
James Gornet <jamesgornet@localhost.localdomain>
2019-05-09 17:51:05 -0400

    Added capability for NeuroTorch for training with TensorFlow

--- neurotorch/core/trainer.py
+++ neurotorch/core/trainer.py
@@ -1,10 +1,5 @@
-import torch
-import torch.optim as optim
-import torch.nn as nn
-from torch.autograd import Variable
-from torch.utils.data import DataLoader
-from neurotorch.datasets.dataset import AlignedVolume, TorchVolume
-import torch.cuda
+import tensorflow as tf
+from neurotorch.datasets.dataset import TorchVolume
 import numpy as np
 
 
@@ -34,12 +29,12 @@ class Trainer(object):
             self.net.load_state_dict(torch.load(checkpoint))
 
         if optimizer is None:
-            self.optimizer = optim.Adam(self.net.parameters())
+            self.optimizer = tf.train.AdamOptimizer()
         else:
-            self.optimizer = optimizer(self.net.parameters())
+            self.optimizer = optimizer()
 
         if criterion is None:
-            self.criterion = nn.BCEWithLogitsLoss()
+            self.criterion = tf.nn.sigmoid_cross_entropy_with_logits()
         else:
             self.criterion = criterion
 
@@ -197,6 +192,198 @@ class TrainerDecorator(Trainer):
                           "Loss: {:.4f}".format(loss),
                           "Accuracy: {:.2f}".format(accuracy*100))
                     self.getTrainer().volume.getVolume().setAugmentation(True)
+
+                num_iter += 1
+
+            num_epoch += 1
+
+
+class TFTrainer(object):
+    """
+    Trains a Tensorflow neural network with a given input and label dataset
+    """
+    def __init__(self, net, aligned_volume, checkpoint=None,
+                 optimizer=None, criterion=None, max_epochs=10,
+                 gpu_device=None, validation_split=0.2):
+        """
+        Sets up the parameters for training
+
+        :param net: A PyTorch neural network
+        :param inputs_volume: A PyTorch dataset containing inputs
+        :param labels_volume: A PyTorch dataset containing corresponding labels
+        """
+        self.max_epochs = max_epochs
+
+        self.net = net
+
+        if checkpoint is not None:
+            self.checkpoint = checkpoint
+
+        if optimizer is None:
+            self.optimizer = tf.train.AdamOptimizer()
+        else:
+            self.optimizer = optimizer()
+
+        if criterion is None:
+            self.criterion = tf.losses.sigmoid_cross_entropy
+        else:
+            self.criterion = criterion
+
+        if gpu_device is not None:
+            self.gpu_device = gpu_device
+            self.useGpu = True
+
+        self.volume = TorchVolume(aligned_volume)
+
+    def run_epoch(self, sample_batch):
+        """
+        Runs an epoch with a given batch of samples
+
+        :param sample_batch: A dictionary containing inputs and labels with the keys 
+"input" and "label", respectively
+        """
+        with tf.GradientTape() as tape:
+            inputs = tf.convert_to_tensor(sample_batch[0],
+                                          dtype=tf.float32)
+            labels = tf.convert_to_tensor(sample_batch[1],
+                                          dtype=tf.float32)
+
+            outputs = self.net(inputs)
+
+            loss = self.criterion(labels, outputs)
+
+        variables = self.net.variables
+        gradients = tape(loss, self.net.variables)
+        self.optimizer.apply_gradients(zip(gradients, variables))
+
+        return loss
+
+    def evaluate(self, batch):
+        inputs = tf.convert_to_tensor(batch[0],
+                                      dtype=tf.float32)
+        labels = tf.convert_to_tensor(batch[1],
+                                      dtype=tf.float32)
+
+        outputs = self.net(inputs)
+
+        loss = self.criterion(labels, outputs)
+        accuracy = tf.sum((outputs > 0) & (labels > 0))
+        accuracy /= tf.sum((outputs > 0) | (labels > 0))
+
+        return loss, accuracy, tf.stack(outputs)
+
+    def run_training(self):
+        """
+        Trains the given neural network
+        """
+        num_epoch = 1
+        num_iter = 1
+
+        validation_split = 0.2
+        valid_indexes = self.getTrainer().volume.getValidData()
+        random_idx = np.random.permutation(valid_indexes)
+        train_idx = random_idx[:int(len(valid_indexes)*(1-validation_split))].copy()
+        val_idx = random_idx[int(len(valid_indexes)*validation_split):].copy()
+
+        train_idx = train_idx[:(len(train_idx) - len(train_idx) % 16)]
+        train_idx = train_idx.reshape((-1, 16))
+
+        while num_epoch <= self.getTrainer().max_epochs:
+            np.random.shuffle(train_idx)
+            for i in range(train_idx.shape[0]):
+                sample_batch = list(zip(*[self.getTrainer().volume[idx]
+                                          for idx in train_idx[i]]))
+                sample_batch = [np.stack(batch) for batch in sample_batch]
+                sample_batch[1] = sample_batch[1] > 0
+                if num_epoch > self.getTrainer().max_epochs:
+                    break
+                if (sample_batch[1] == 0).all():
+                    continue
+
+                print("Iteration: {}".format(num_iter))
+                self.run_epoch(sample_batch)
+
+                if num_iter % 10 == 0:
+                    val_batch = list(zip(*[self.getTrainer().volume[idx]
+                                           for idx in val_idx[:1]]))
+                    val_batch = [np.stack(batch) for batch in val_batch]
+                    val_batch[1] = val_batch[1] > 0
+                    loss, accuracy, _ = self.evaluate(val_batch)
+                    print("Iteration: {}".format(num_iter),
+                          "Epoch {}/{} ".format(num_epoch,
+                                                self.getTrainer().max_epochs),
+                          "Loss: {:.4f}".format(loss),
+                          "Accuracy: {:.2f}".format(accuracy*100))
+
+                num_iter += 1
+
+            num_epoch += 1
+
+
+class TFTrainerDecorator(Trainer):
+    """
+    A wrapper class to a features for training
+    """
+    def __init__(self, trainer):
+        self.setTrainer(trainer)
+
+    def setTrainer(self, trainer):
+        self._trainer = trainer
+
+    def getTrainer(self):
+        if isinstance(self._trainer, TFTrainerDecorator) or issubclass(type(self._trainer), TFTrainerDecorator):
+            return self._trainer.getTrainer()
+        if isinstance(self._trainer, TFTrainer):
+            return self._trainer
+
+    def run_epoch(self, sample_batch):
+        return self._trainer.run_epoch(sample_batch)
+
+    def evaluate(self, batch):
+        return self._trainer.evaluate(batch)
+
+    def run_training(self):
+        """
+        Trains the given neural network
+        """
+        num_epoch = 1
+        num_iter = 1
+
+        validation_split = 0.2
+        valid_indexes = self.getTrainer().volume.getValidData()
+        random_idx = np.random.permutation(valid_indexes)
+        train_idx = random_idx[:int(len(valid_indexes)*(1-validation_split))].copy()
+        val_idx = random_idx[int(len(valid_indexes)*validation_split):].copy()
+
+        train_idx = train_idx[:(len(train_idx) - len(train_idx) % 16)]
+        train_idx = train_idx.reshape((-1, 16))
+
+        while num_epoch <= self.getTrainer().max_epochs:
+            np.random.shuffle(train_idx)
+            for i in range(train_idx.shape[0]):
+                sample_batch = list(zip(*[self.getTrainer().volume[idx]
+                                          for idx in train_idx[i]]))
+                sample_batch = [np.stack(batch) for batch in sample_batch]
+                sample_batch[1] = sample_batch[1] > 0
+                if num_epoch > self.getTrainer().max_epochs:
+                    break
+                if (sample_batch[1] == 0).all():
+                    continue
+
+                print("Iteration: {}".format(num_iter))
+                self.run_epoch(sample_batch)
+
+                if num_iter % 10 == 0:
+                    val_batch = list(zip(*[self.getTrainer().volume[idx]
+                                           for idx in val_idx[:1]]))
+                    val_batch = [np.stack(batch) for batch in val_batch]
+                    val_batch[1] = val_batch[1] > 0
+                    loss, accuracy, _ = self.evaluate(val_batch)
+                    print("Iteration: {}".format(num_iter),
+                          "Epoch {}/{} ".format(num_epoch,
+                                                self.getTrainer().max_epochs),
+                          "Loss: {:.4f}".format(loss),
+                          "Accuracy: {:.2f}".format(accuracy*100))
 
                 num_iter += 1
 
